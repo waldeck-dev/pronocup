73661b3f3ac2235566537b5bf3cc9192c90b6531
Valentin <valentin@waldeck.dev>
2022-09-07 17:28:19 +0200

    Create UserGroup entry upon group creation

--- back/src/api/group/controllers/group.js
+++ back/src/api/group/controllers/group.js
@@ -6,4 +6,18 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::group.group');
+module.exports = createCoreController('api::group.group', ({ strapi }) => ({
+  /**
+   * Create UserGroup object upon Group.create()
+   */
+  async create(ctx) {
+    const response = await super.create(ctx);
+    
+    const group = response.data.id;
+    const data = { group, user: ctx.state.user.id };
+
+    await strapi.entityService.create('api::user-group.user-group', { data });
+
+    return response;
+  }
+}));

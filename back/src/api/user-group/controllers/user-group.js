31753a49881720579ea47db72e99aa704f8f6295
Valentin <valentin@waldeck.dev>
2022-09-07 11:26:48 +0200

    Add UserGroup model

---
+++ back/src/api/user-group/controllers/user-group.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-group controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::user-group.user-group');
---
+++ back/src/api/user-group/routes/user-group.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-group router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::user-group.user-group');

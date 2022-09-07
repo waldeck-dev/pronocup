cc57cee1a3af6b9c9640a6c45147a27ddf70ce75
Valentin <valentin@waldeck.dev>
2022-09-07 19:03:25 +0200

    Add update policy

--- back/src/api/group/routes/group.js
+++ back/src/api/group/routes/group.js
@@ -6,4 +6,23 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::group.group');
+module.exports = createCoreRouter('api::group.group', {
+  config: {
+    update: {
+      policies: [
+        {
+          name: 'global::is-owner',
+          config: {
+            owner: async ({ ctx, strapi }) => {
+              const entities = await strapi.entityService.findMany('api::user-group.user-group', {
+                filters:  { group: +ctx.params.id, user: ctx.state.user.id }
+              });
+
+              if (entities.length === 1) return ctx.state.user.id;
+            }
+          }
+        }
+      ]
+    }
+  }
+});

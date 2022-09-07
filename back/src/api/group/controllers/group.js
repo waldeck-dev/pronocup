276306a6008f980924fc06ccc8ee7bb1cde12700
Valentin <valentin@waldeck.dev>
2022-09-07 18:10:54 +0200

    Add group.delete() logic

--- back/src/api/group/controllers/group.js
+++ back/src/api/group/controllers/group.js
@@ -14,9 +14,29 @@ module.exports = createCoreController('api::group.group', ({ strapi }) => ({
     const response = await super.create(ctx);
     
     const group = response.data.id;
-    const data = { group, user: ctx.state.user.id };
+    const data = { group, user: ctx.state.user.id, confirmed: true };
 
     await strapi.entityService.create('api::user-group.user-group', { data });
+
+    return response;
+  },
+
+  /**
+   * Delete UserGroup object upon Group.delete()
+   */
+  async delete(ctx) {
+    console.log('BEFORE', ctx.params);
+    const group = +ctx.params.id;
+
+    const entities = await strapi.entityService.findMany('api::user-group.user-group', {
+      filters: { user: ctx.state.user.id, group }
+    });
+
+    const response = await super.delete(ctx);
+
+    for (const entity of entities) {
+      await strapi.entityService.delete('api::user-group.user-group', entity.id);
+    }
 
     return response;
   }

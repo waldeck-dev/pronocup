9d6d72516db05ad5d445177cc7bc782229cc2bae
Valentin <valentin@waldeck.dev>
2022-09-11 13:50:45 +0200

    Test getAuthenticatedUser() util func

--- back/tests/app.test.js
+++ back/tests/app.test.js
@@ -1,4 +1,5 @@
-const { setupStrapi, cleanupStrapi } = require('./helpers/strapi');
+const { setupStrapi, cleanupStrapi, createUser } = require('./helpers/strapi');
+const { getAuthenticatedUser, extractData } = require('../src/utils');
 
 jest.setTimeout(30000);
 
@@ -12,4 +13,22 @@ afterAll(async () => {
 
 test('strapi is defined', () => {
   expect(strapi).toBeDefined();
+});
+
+/**
+ * Test utils.js
+ */
+describe('Test utils.js functions', () => {
+
+  test('getAuthenticatedUser works properly', async () => {
+    // User NOT exists
+    expect(await getAuthenticatedUser(-1)).toBe(null);
+
+    // User exists
+    const { user } = await createUser();
+    const authUser = await getAuthenticatedUser(user.id);
+    expect(typeof authUser).toBe('object');
+    expect(authUser.id).toBe(user.id);
+  });
+
 });

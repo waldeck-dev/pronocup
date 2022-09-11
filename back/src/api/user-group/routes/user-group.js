dbb7e67499af42aedfd80109794858f317fc47f0
Valentin <valentin@waldeck.dev>
2022-09-11 09:00:46 +0200

    Add user-group create logic

--- back/src/api/user-group/routes/user-group.js
+++ back/src/api/user-group/routes/user-group.js
@@ -6,4 +6,21 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::user-group.user-group');
+module.exports = createCoreRouter('api::user-group.user-group', {
+  config: {
+    create: {
+      policies: [
+        {
+          name: 'global::is-owner',
+          config: {
+            negate: true,
+            getObject: async ({ ctx, strapi }) => await strapi.entityService
+              .findOne('api::group.group', ctx.request.body.data.group, {
+                populate: { owner: true }
+              })
+          }
+        }
+      ]
+    }
+  }
+});

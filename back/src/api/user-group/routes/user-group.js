8f6b2f31879c0ed0e70ae5f390323444d294a46f
Valentin <valentin@waldeck.dev>
2022-09-11 09:17:54 +0200

    Add user-group update logic

--- back/src/api/user-group/routes/user-group.js
+++ back/src/api/user-group/routes/user-group.js
@@ -8,6 +8,9 @@ const { createCoreRouter } = require('@strapi/strapi').factories;
 
 module.exports = createCoreRouter('api::user-group.user-group', {
   config: {
+    /**
+     * CREATE: Join request from other user
+     */
     create: {
       policies: [
         {
@@ -18,6 +21,30 @@ module.exports = createCoreRouter('api::user-group.user-group', {
               .findOne('api::group.group', ctx.request.body.data.group, {
                 populate: { owner: true }
               })
+          }
+        }
+      ]
+    },
+
+    /**
+     * UPDATE: Group owner accept / reject members
+     */
+    update: {
+      policies: [
+        {
+          name: 'global::is-owner',
+          config: {
+            getObject: async ({ ctx, strapi }) => {
+              const userGroup = await strapi.entityService
+                .findOne('api::user-group.user-group', ctx.params.id, {
+                  populate: { group: true }
+                });
+
+              return await strapi.entityService
+                .findOne('api::group.group', userGroup.group.id, {
+                  populate: { owner: true }
+                });
+            }
           }
         }
       ]

10537cad93379b41b03f2b46b5e60fd58685a24c
Valentin <valentin@waldeck.dev>
2022-09-16 21:56:28 +0200

    Add field filtering to UserGroup update

--- back/src/api/user-group/controllers/user-group.js
+++ back/src/api/user-group/controllers/user-group.js
@@ -20,6 +20,19 @@ module.exports = createCoreController('api::user-group.user-group', ({ strapi })
     const userGroup = await strapi.entityService
       .create('api::user-group.user-group', { data });
 
-    if (userGroup) return ctx.send({ data: userGroup }, 201);
+    if (userGroup) return ctx.send({ data: userGroup, meta: {} }, 201);
+  },
+
+  /**
+   * UPDATE: Group owner accept / reject members
+   */
+  async update(ctx) {
+    if (ctx.request.body.data) {
+      ctx.request.body.data = {
+        ...extractData(ctx.request.body.data, ['confirmed', 'blocked'])
+      };
+    }
+
+    return await super.update(ctx);
   }
 }));

93dc97efa539805cc6cfcf0cffc21fb6d2b792e9
Valentin <valentin@waldeck.dev>
2022-09-17 15:52:29 +0200

    Add pronostic model

---
+++ back/src/api/pronostic/controllers/pronostic.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * pronostic controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::pronostic.pronostic');
---
+++ back/src/api/pronostic/routes/pronostic.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * pronostic router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::pronostic.pronostic');

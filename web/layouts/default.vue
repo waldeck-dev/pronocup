cf221feae17c33c215ef9e8fe5119e0463089e29
Valentin <valentin@waldeck.dev>
2022-11-01 17:33:22 +0100

    Init nuxt project

---
+++ web/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container column is-10">
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout'
+}
+</script>
---
+++ web/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>The Index</div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>

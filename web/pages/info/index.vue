6f446c91ed4c919229ab8ee60d16b6fa3cbf4389
Valentin <valentin@waldeck.dev>
2022-11-05 09:10:18 +0100

    Tweak info page

--- web/pages/info/index.vue
+++ web/pages/info/index.vue
@@ -1,13 +1,22 @@
 <template>
-  <div>
+  <div class="has-text-centered">
+    <SectionTitle>Gestion de mon compte</SectionTitle>
+
     <b-button rounded @click="$router.push({ name: 'logout' })">
       👋 Se déconnecter
+    </b-button>
+
+    <b-button class="mt-2" type="is-danger" rounded disabled>
+      🗑️ Supprimer mon compte
     </b-button>
   </div>
 </template>
 
 <script>
+import SectionTitle from '@/components/ui/SectionTitle.vue'
+
 export default {
   name: 'InfoPage',
+  components: { SectionTitle }
 }
 </script>
